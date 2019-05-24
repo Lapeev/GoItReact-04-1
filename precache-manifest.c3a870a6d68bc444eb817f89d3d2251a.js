5478a11f8d48b7bdf9b443d2714dc499a0a7db3e
lapeev16@gmail.com <lapeev16@gmail.com>
2019-05-24 16:27:03 +0300

    Updates

--- precache-manifest.c3a870a6d68bc444eb817f89d3d2251a.js
+++ precache-manifest.c3a870a6d68bc444eb817f89d3d2251a.js
@@ -4,59 +4,59 @@ self.__precacheManifest = [
     "url": "/GoItReact-04-1/static/css/4.acc4478e.chunk.css"
   },
   {
-    "revision": "dd5f478f015a9c8fc58b",
+    "revision": "6dd5e06cc5baf7132de8",
     "url": "/GoItReact-04-1/static/css/main.721778ca.chunk.css"
   },
   {
-    "revision": "c7390b7325ff028d1e6c",
-    "url": "/GoItReact-04-1/static/js/runtime~main.d915278f.js"
+    "revision": "ee2b7c437c8964919d83",
+    "url": "/GoItReact-04-1/static/js/runtime~main.cc0e5399.js"
   },
   {
     "revision": "1130f550f747b5f1fac5d4f701bc9ba3",
     "url": "/GoItReact-04-1/static/media/logo.1130f550.png"
   },
   {
-    "revision": "cb6ccb6a0f08aaeb863d",
-    "url": "/GoItReact-04-1/static/js/2.3fe0c443.chunk.js"
+    "revision": "331616d15e40ea63b886",
+    "url": "/GoItReact-04-1/static/js/2.0bc051e0.chunk.js"
   },
   {
     "revision": "0fd1f43d977f59d662de",
     "url": "/GoItReact-04-1/static/js/6.0b3d4b53.chunk.js"
   },
   {
-    "revision": "f007294622c3f9c80488",
-    "url": "/GoItReact-04-1/static/js/3.f6e725ca.chunk.js"
+    "revision": "c8501931dff851c0a58a",
+    "url": "/GoItReact-04-1/static/js/3.8419d1b8.chunk.js"
   },
   {
-    "revision": "dd5f478f015a9c8fc58b",
-    "url": "/GoItReact-04-1/static/js/main.4e843e4c.chunk.js"
+    "revision": "6dd5e06cc5baf7132de8",
+    "url": "/GoItReact-04-1/static/js/main.13c4472d.chunk.js"
   },
   {
     "revision": "451f51037e61f2decfb2",
     "url": "/GoItReact-04-1/static/js/4.a145fbec.chunk.js"
   },
   {
-    "revision": "e3d933bf56f431db36ce",
-    "url": "/GoItReact-04-1/static/js/5.ba8fe49d.chunk.js"
+    "revision": "38f8e87ad91e4f9c2eed",
+    "url": "/GoItReact-04-1/static/js/5.7e3eb6c2.chunk.js"
   },
   {
     "revision": "0fd1f43d977f59d662de",
     "url": "/GoItReact-04-1/static/css/6.8fe2d8fc.chunk.css"
   },
   {
-    "revision": "e3d933bf56f431db36ce",
+    "revision": "38f8e87ad91e4f9c2eed",
     "url": "/GoItReact-04-1/static/css/5.7ad5d12f.chunk.css"
   },
   {
-    "revision": "f007294622c3f9c80488",
+    "revision": "c8501931dff851c0a58a",
     "url": "/GoItReact-04-1/static/css/3.eb11ba67.chunk.css"
   },
   {
-    "revision": "cb6ccb6a0f08aaeb863d",
+    "revision": "331616d15e40ea63b886",
     "url": "/GoItReact-04-1/static/css/2.e057c3af.chunk.css"
   },
   {
-    "revision": "a07b830ebf1582ee4f35eedf793a8fe5",
+    "revision": "cfa3b8e67a0b7f59c5f5f9a32486c5c9",
     "url": "/GoItReact-04-1/index.html"
   }
 ];
